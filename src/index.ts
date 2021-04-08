ea73de3b032e105c00b5e43cee34e2a3ad784c01
royjsong <jhsong@selvas.com>
2021-04-09 08:55:40 +0900

    특정 파일 제외하고 소스코드 압축하는 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 import {getInputs} from './input-helper'
 import * as fs from 'fs'
 import dataFormat from 'dateformat'
+import archiver from 'archiver'
 
 async function run(): Promise<void> {
     try {
@@ -9,7 +10,7 @@ async function run(): Promise<void> {
         const versionFilePath = process.env['GITHUB_WORKSPACE'] + "/version.json"
         try {
             if (fs.existsSync(versionFilePath)) {
-                console.log("found version.json.")
+                console.log("found version.json file.")
             }
         } catch(err) {
             console.error("Not Found version.json")
@@ -24,8 +25,33 @@ async function run(): Promise<void> {
         console.log(`gitSha : ${inputs.gitSha}`)
         console.log(`date : ${date}`)
 
-        const packageName = inputs.fileName + "_" + version + "_" + inputs.gitSha.slice(0, 8) + "_" + date
+        const packageName = inputs.fileName + "_" + version + "_" + inputs.gitSha.slice(0, 6) + "_" + date
         core.setOutput("packageName", packageName);
+
+        const archiveIgnorePath = process.env['GITHUB_WORKSPACE'] + '/.archiveignore'
+        try {
+            if (fs.existsSync(archiveIgnorePath)) {
+                console.log("found archiveignore file.")
+            }
+        } catch(err) {
+            console.error("Not Found .archiveignore")
+        }
+
+        const lines: string[] = require('fs').readFileSync(archiveIgnorePath, 'utf-8').split('\n').filter(Boolean);
+        lines.push(packageName + ".zip")
+        console.log(`.achiveignore :  ${lines}`)        
+    
+        const output = fs.createWriteStream(process.env['GITHUB_WORKSPACE'] + '/' + packageName + ".zip")
+        const archive = archiver('zip', {
+            zlib: {level : 9 }
+        })
+        archive.pipe(output);
+        archive.glob('**/*', {
+            cwd: process.env['GITHUB_WORKSPACE'],
+            ignore: lines,
+            dot: true,
+        });
+        archive.finalize();
     } catch (err) {
         core.setFailed(err.message)
     }
